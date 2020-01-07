37ea641f539950d2dcb84f47d8a9f652e5ac33cb
calvinrzachman <calvin.zachman@protonmail.com>
2020-01-06 20:02:28 -0600

    integrate StreamPaymentRequest RPC
    - PaymentProcessor implements PaymentsServer interface with StreamPaymentRequests RPC
    - begin cleanup
    - add folder of repository at historical points of interest

--- client/main.go
+++ client/main.go
@@ -1,26 +1,55 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
+	"os/user"
+	"path"
 	"time"
 
+	"projects/bitstream/rpc"
+
+	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 // Bitstream Client
 var (
-	dataWindow   time.Duration = 15 * time.Second
-	pauseTimeout time.Duration = 5 * time.Second
+	dataWindow      time.Duration = 15 * time.Second
+	pauseTimeout    time.Duration = 5 * time.Second
+	bitstreamClient BitstreamClient
 )
 
-func main() {
-	// ctx := context.Background()
-	// ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
-	// defer cancel()
+// BitstreamClient represents a client of the BitstreamServer
+// which streams payments via Lightning Network in exchange for random data (for now!)
+type BitstreamClient struct {
+	LightningClient lnrpc.LightningClient
+	ID              string
+	// BytesStreamConn *net.TCPConn
+	// BitstreamInvoiceStream *grpc.ClientConn
+}
 
+func init() {
+	// Initialize the Bitstream Payment Streamer
+	lightningClient, err := EnableLightning()
+	if err != nil {
+		log.Fatalf("[PaymentStreamer init]: unable to establish connection to lnd RPC server: %v", err)
+	}
+
+	bitstreamClient = BitstreamClient{
+		LightningClient: lightningClient,
+	}
+}
+
+func main() {
+
+	// Stream random bytes from Bitstream server using raw TCP connection
 	tcpAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8080")
 	if err != nil {
 		log.Fatal(errors.Wrap(err, "unable to resolve TCP address"))
@@ -29,14 +58,29 @@ func main() {
 	if err != nil {
 		log.Fatal(errors.Wrap(err, "unable to establish TCP connection to server"))
 	}
+	defer conn.Close()
 
-	defer conn.Close() // IMPORTANT: Connections must be closed otherwise they will hang around in CLOSE_WAIT
+	// Connects to our (Bitstream) gRPC server.
+	gRPCConn, err := grpc.Dial(":8081", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("unable to establish connection to :8081: %v", err)
+	} else {
+		log.Println("connection established")
+	}
+	defer conn.Close()
 
-	// go streamPayment()
+	// Stream Lightning Network payment requests from Bitstream server (gRPC)
+	// and send a stream of payments via Bitstream client's lightning node
+	log.Println("Connected to Bitstream using local address: ", conn.LocalAddr())
+	bitstreamClient.ID = conn.LocalAddr().String()
+	go bitstreamClient.streamPayment(gRPCConn)
 
 	// Stream data from Bitstream server
 	streamRandomData(conn)
 
+	// NOTE: We might need to add a way to communicate between the main go routine streaming random
+	// bytes and the go routine streaming payment so that we can discontinue payment should we
+	// stop receiving data. This seems like Flow Control. Is this neccessary??
 }
 
 func streamRandomData(conn net.Conn) {
@@ -46,9 +90,9 @@ func streamRandomData(conn net.Conn) {
 		n, err := conn.Read(buf[:])
 		if err != nil {
 			if err == io.EOF {
-				log.Println("\nThe stream has ended. Closing connection to Bitstream server")
+				log.Println("[inside streamRandomData]: The server has ended the stream. Closing connection to Bitstream server")
 			} else {
-				log.Printf("Finished with error: %v", err)
+				log.Printf("[inside streamRandomData]: Finished with error: %v", err)
 			}
 			return
 		}
@@ -62,41 +106,83 @@ func streamRandomData(conn net.Conn) {
 	}
 }
 
+func (bitstream *BitstreamClient) streamPayment(conn *grpc.ClientConn) {
+	// continueStream := make(chan bool)
+	// SendPayment RPC https://github.com/lightningnetwork/lnd/tree/master/lnrpc
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// Generates a lightning invoice client to stream payment requests from
+	// Bitstream server (with all the functionality we expect as defined in the protobuf)
+	client := rpc.NewPaymentsClient(conn)
+
+	// Stream Payment requests from Bitstream server
+	stream, err := client.StreamPaymentRequests(ctx, &rpc.StreamPaymentRequest{
+		ClientID: bitstream.ID,
+	})
+	if err != nil {
+		log.Fatalf("[inside streamPayment]: unable to obtain stream: %v", err)
+	}
+
+	// Stream payment to Bitstream server
+	for {
+		// Await receipt of payment request from Bitstream server
+		paymentRequest, err := stream.Recv()
+		if err == io.EOF {
+			// cancel()
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.StreamPaymentRequests(_) = _, %v", client, err)
+			break
+		}
+		// log.Println("[inside streamPayment]: Payment request received! ", paymentRequest)
+
+		// Send Lightning Payment to Bitstream server
+		// TODO: Investigate use of SendPayment RPC for bidirectional payment streams
+		_, payErr := bitstream.LightningClient.SendPaymentSync(context.Background(), &lnrpc.SendRequest{
+			PaymentRequest: paymentRequest.PaymentRequest,
+		})
+		if payErr != nil {
+			log.Println("[inside streamPayment]: Unable to complete payment!!! ", payErr)
+		}
+		// select {
+		// case <-continueStream:
+		// }
+	}
+}
+
+// EnableLightning opens a gRPC connection to lightning network daemon backend
+func EnableLightning() (lnrpc.LightningClient, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return nil, fmt.Errorf("Cannot get current user: %v", err)
+	}
+	// fmt.Println("The user home directory: " + usr.HomeDir)
+	tlsCertPath := path.Join(usr.HomeDir, "Library/Application Support/Lnd/tls.cert")
+	// macaroonPath := path.Join(usr.HomeDir, ".lnd/admin.macaroon")
+
+	tlsCreds, err := credentials.NewClientTLSFromFile(tlsCertPath, "")
+	if err != nil {
+		return nil, fmt.Errorf("Cannot get node tls credentials: %v", err)
+	}
+
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(tlsCreds),
+		grpc.WithBlock(),
+		// grpc.WithPerRPCCredentials(macaroons.NewMacaroonCredential(mac)),
+	}
+
+	conn, err := grpc.Dial("localhost:10003", opts...)
+	if err != nil {
+		return nil, fmt.Errorf("cannot dial lnd backend: %v", err)
+	}
+
+	client := lnrpc.NewLightningClient(conn)
+	return client, nil
+}
+
 // func lastReceivedData() time.Time {
 // 	return time.Now()
-// }
-
-// // LightningInvoice is the Bitstream client's
-// // representation of Lighning Network invoices
-// type LightningInvoice struct {
-// 	invoice lnrpc.Invoice
-// }
-
-// // InvoiceStreamer is a gRPC client stream which receives
-// // a stream on Lightning Network payment invoices from the Bitsream server
-// type InvoiceStreamer struct {
-// 	Invoices grpc.ClientStream
-// }
-
-// func streamPayment() {
-// 	continueStream := make(chan bool)
-// 	// SendPayment RPC https://github.com/lightningnetwork/lnd/tree/master/lnrpc
-
-// 	// Stream invoices from Bitstream server
-// 	var invoice LightningInvoice
-// 	go func() {
-// 		for {
-// 			// invoice, err := grpc.blah blah blah.Recv(&invoice)
-// 		}
-// 	}()
-
-// 	// Send Lightning network payment to Bitstream server
-
-// 	// Stream payment to Bitstream server
-// 	for {
-
-// 		select {
-// 		case <-continueStream:
-// 		}
-// 	}
 // }
--- go.mod
+++ go.mod
@@ -1,5 +1,22 @@
-module projects/go_learning/random-server
+module projects/bitstream
 
-go 1.12
+go 1.13
 
-require github.com/pkg/errors v0.8.1
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/btcsuite/go-flags v0.0.0-20150116065318-6c288d648c1c // indirect
+	github.com/codahale/chacha20 v0.0.0-20151107025005-ec07b4f69a3f // indirect
+	github.com/codahale/chacha20poly1305 v0.0.0-20151127064032-f8a5c4830182 // indirect
+	github.com/davecgh/go-spew v1.1.1
+	github.com/golang/protobuf v1.3.2
+	github.com/howeyc/gopass v0.0.0-20190910152052-7cb4b85ec19c // indirect
+	github.com/lightningnetwork/lightning-onion v0.0.0-20191214001659-f34e9dc1651d // indirect
+	github.com/lightningnetwork/lnd v0.8.2-beta
+	github.com/pkg/errors v0.8.1
+	github.com/roasbeef/btcd v0.0.0-20180418012700-a03db407e40d // indirect
+	github.com/roasbeef/btcrpcclient v0.0.0-20170622074026-d0f4db8b4dad // indirect
+	github.com/roasbeef/btcutil v0.0.0-20180406014609-dfb640c57141 // indirect
+	github.com/roasbeef/btcwallet v0.0.0-20180426223453-30affec83c18 // indirect
+	google.golang.org/grpc v1.26.0
+	gopkg.in/macaroon.v2 v2.1.0
+)
